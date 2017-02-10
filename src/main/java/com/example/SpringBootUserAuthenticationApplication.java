a88eef63edac92d4d1f290774f65cb825f548cff
Krishna Bahadur Bhat <krishna1bhat@Krishnas-MacBook-Pro.local>
2017-02-10 14:13:06 -0600

    First commit from local machine

---
+++ src/main/java/com/example/SpringBootUserAuthenticationApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUserAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootUserAuthenticationApplication.class, args);
+	}
+}
